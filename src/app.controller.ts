1b2521061feaac8f7b2f389d8aceefe2463b2d0a
Simplecodez <gozieelendu@gmail.com>
2025-04-20 21:31:26 +0100

    chore: add api documentation decorators to app controller methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Returns a welcome message'
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns a string'
+  })
   getHello(): string {
     return this.appService.getHello();
   }
